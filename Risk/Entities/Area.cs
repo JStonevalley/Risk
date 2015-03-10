25037a16b4bffdccd77fa7b2289741e7665fe030
Jonas Stendahl <jonas.stenahl@outlook.com>
2015-03-11 00:02:23 +0100

    Fixed Territories in Area

---
+++ Risk/Entities/Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk
+{
+    public class Area
+    {
+        public Area() { }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int TroopBonus { get; set; }
+        public ICollection<Territory> Territories { get; set; }
+    }
+}
